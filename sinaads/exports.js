582ea9427deb37a37d2087874319d81787d5bcd7
acelan86 <rabbitlxb@qq.com>
2014-02-16 21:34:20 +0800

    update

--- sinaads/exports.js
+++
@@ -1,5 +0,0 @@
-//导出一些变量
-window.sinaadsROC = controllerModule.orderController;
-window.sinaadsRFC = controllerModule.frequenceController;
-window._sinaadsCacheData = modelModule.get();
-window.sinaadsRenderHandler = viewModule.handlerMap;
