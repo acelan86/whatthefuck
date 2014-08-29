1f63d6180b725f8290c68170dd8fdba88e3096bd
acelan86 <79480491@qq.com>
2014-08-29 13:40:49 +0800

    acelan

--- src/spec/fuyi_thirdpart_code.js
+++ src/spec/fuyi_thirdpart_code.js
@@ -1,7 +1,10 @@
 (function (d, s, id) {
+    "use strict";
     var uid = "woshiacelanaacelan";
     var code = "//d5.sina.com.cn/litong/zhitou/sinaads/src/spec/fuyi_small_thirdpart_code.html?" + uid;
-    if (document.getElementById(id)) return;
+    if (document.getElementById(id)) {
+        return;
+    }
     var iframe = document.createElement('iframe');
     iframe.src = code;
     iframe.id = id;
