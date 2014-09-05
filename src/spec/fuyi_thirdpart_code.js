698759dc2fc83c61c8b4e4174bfbed93a684f76c
acelan86 <79480491@qq.com>
2014-09-05 15:18:18 +0800

    acelan

--- src/spec/fuyi_thirdpart_code.js
+++ src/spec/fuyi_thirdpart_code.js
@@ -1,13 +1,12 @@
-(function (d, s, id) {
-    "use strict";
-    var uid = "woshiacelanaacelan";
-    var code = "//r.sax.sina.com.cn/fy.html?" + uid;
-    if (document.getElementById(id)) {
-        return;
-    }
-    var iframe = document.createElement('iframe');
-    iframe.src = code;
-    iframe.id = id;
-    iframe.style.cssText = 'position:absolute;width:1px;height:1px;border:none;';
-    document.body.appendChild(iframe);
-})(document, 'iframe', 'sina-fy');
+(function (d, id) {
+    var uid = "5202423375",
+        code = "//r.sax.sina.com.cn/fy.html?" + uid,
+        n = d.getElementsByTagName('script')[0],
+        s;
+    if (document.getElementById(id)) return;
+    s = d.createElement('iframe');
+    s.style.cssText = "position:absolute;width:1px;height:1px;margin:0px;padding:0px;overflow:hidden;border:none;"
+    s.src = code;
+    s.id = id;
+    n.parentNode.insertBefore(s, n);
+})(document, 'sina-fy');
