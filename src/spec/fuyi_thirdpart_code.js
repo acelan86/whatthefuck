bc822fdb02a6da414db260332a09939358c35e93
acelan86 <79480491@qq.com>
2014-09-05 16:27:51 +0800

    acelan

---
+++ src/spec/fuyi_thirdpart_code.js
@@ -0,0 +1,12 @@
+(function (d, id) {
+    var uid = "5202423375",
+        code = "//r.sax.sina.com.cn/fy.html?" + uid,
+        n = d.getElementsByTagName('script')[0],
+        s;
+    if (document.getElementById(id)) return;
+    s = d.createElement('iframe');
+    s.style.cssText = "position:absolute;width:1px;height:1px;margin:0px;padding:0px;overflow:hidden;border:none;"
+    s.src = code;
+    s.id = id;
+    n.parentNode.insertBefore(s, n);
+})(document, 'sina-fy');
