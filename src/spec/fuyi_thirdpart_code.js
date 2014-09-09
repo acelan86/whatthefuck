bee09b121afc12d630c4b14a0857ce901552487c
acelan86 <79480491@qq.com>
2014-09-09 10:46:32 +0800

    acelan

--- src/spec/fuyi_thirdpart_code.js
+++ src/spec/fuyi_thirdpart_code.js
@@ -1,11 +1,14 @@
 (function (d, id) {
+    "use strict";
     var uid = "5202423375",
         code = "//r.sax.sina.com.cn/fy.html?" + uid,
         n = d.getElementsByTagName('script')[0],
         s;
-    if (document.getElementById(id)) return;
+    if (document.getElementById(id)) {
+        return;
+    }
     s = d.createElement('iframe');
-    s.style.cssText = "position:absolute;width:1px;height:1px;margin:0px;padding:0px;overflow:hidden;border:none;"
+    s.style.cssText = "position:absolute;width:1px;height:1px;margin:0px;padding:0px;overflow:hidden;border:none;";
     s.src = code;
     s.id = id;
     n.parentNode.insertBefore(s, n);
